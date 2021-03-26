52dbab1a2fb23cb8fe198f285ff4663ab3271131
techlord <pgmpofu@gmail.com>
2021-03-25 22:53:09 -0700

    Simulate rolling a pair of dice

---
+++ src/RollDice.java
@@ -0,0 +1,12 @@
+/**
+ * Simulator for rolling a pair of dice
+ */
+public class RollDice {
+    public static void main(String[] args) {
+        int roll1 = (int)(Math.random()*6) + 1;
+        int roll2 = (int)(Math.random()*6) + 1;
+        System.out.println("Dice 1 " + roll1);
+        System.out.println("Dice 2 " + roll2);
+        System.out.println("Total  " + (roll1 + roll2));
+    }
+}
