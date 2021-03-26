97c4405a19670de1e64513c35f3a9254ebd7f86e
techlord <pgmpofu@gmail.com>
2021-03-25 22:43:14 -0700

    Print your initials

---
+++ src/PrintInitials.java
@@ -0,0 +1,18 @@
+/**
+ * Write a program that will print your initials to standard output
+ * in letters that are nine lines tall. Each big letter should be
+ * made up of a bunch of *'s.
+ */
+public class PrintInitials {
+    public static void main(String[] args) {
+        System.out.println("******           *************        **********\n" +
+                "**    **                **            **\n" +
+                "**     **               **            **\n" +
+                "**      **              **            **\n" +
+                "**      **              **            ********\n" +
+                "**      **       **     **            **\n" +
+                "**     **         **    **            **\n" +
+                "**    **           **  **             **\n" +
+                "*****               ****              ********** ");
+    }
+}
