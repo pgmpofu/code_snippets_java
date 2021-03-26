f477a1d8e6bca48ccd3797a8e84776c4032b8cfd
techlord <pgmpofu@gmail.com>
2021-03-25 23:30:51 -0700

    Count change in dollars

---
+++ src/ChangeCounter.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ChangeCounter {
+    public static void main(String[] args) {
+        System.out.println("How many quarters do you have?");
+        Scanner scanner = new Scanner(System.in);
+        int quarters = scanner.nextInt();
+        System.out.println("How many dimes do you have?");
+        int dimes = scanner.nextInt();
+        System.out.println("How many nickels do you have?");
+        int nickels = scanner.nextInt();
+        System.out.println("How many pennies do you have?");
+        int pennies = scanner.nextInt();
+
+        System.out.println("Quarters      :" + (quarters*25));
+        System.out.println("Dimes         :" + (dimes*10));
+        System.out.println("Nickels       :" + (nickels*5));
+        System.out.println("Pennies       :" + pennies);
+        int total = (quarters*25) + (dimes*10) + (nickels*5) + pennies;
+        System.out.println("Total change  :" + BigDecimal.valueOf(total).divide(BigDecimal.valueOf(100)));
+    }
+}
--- src/NameReader.java
+++ src/NameReader.java
@@ -1,10 +1,18 @@
+/**
+ * Write a program that asks the user's name,
+ * and then greets the user by name. Before
+ * outputting the user's name, convert it to upper case letters.
+ */
+
 import java.util.Scanner;
 
 public class NameReader {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("What is your name?");
         while (scanner.hasNext()) {
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name.toUpperCase() + " nice to meet you");
+            String name = scanner.nextLine();
+            System.out.println("Hello " + name.toUpperCase() + " nice to meet you");
+        }
     }
 }
