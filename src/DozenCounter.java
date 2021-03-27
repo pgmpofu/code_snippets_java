65d76f79740969682a69c5fb9a794c9c1402ce1b
techlord <pgmpofu@gmail.com>
2021-03-26 21:36:20 -0700

    Add gitignore, count the number of gross, dozens and remainders in a given number

---
+++ src/DozenCounter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ *A program that asks the user how many eggs she has
+ * and then tells the user how many dozen eggs she has
+ * and how many extra eggs are left over.
+ */
+public class DozenCounter {
+
+    public static void main(String[] args) {
+        final int dozen = 12;
+        final int gross = 144;
+
+        System.out.println("How many eggs do you have?");
+
+        Scanner scanner = new Scanner(System.in);
+        int numOfEggs = scanner.nextInt();
+
+        System.out.println("You have " + (numOfEggs/gross) + " gross");
+        System.out.println("You have " + (numOfEggs/dozen) + " dozens");
+        System.out.println("You have " + (numOfEggs%dozen) + " remaining");
+    }
+}
