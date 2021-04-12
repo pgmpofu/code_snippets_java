141c7a74dd2e390df40e73f1e0fdb1682781abd2
techlord <pgmpofu@gmail.com>
2021-04-12 07:14:14 -0700

    Refactor code to latest syntax

--- src/HelpMenu.java
+++ src/HelpMenu.java
@@ -1,8 +1,3 @@
-/**
- * Menu based help system on Java flow of control
- * @author Patience Mpofu
- * @date 04/11/2021
- */
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -17,23 +12,12 @@ public class HelpMenu {
             try {
                 int option = scanner.nextInt();
                 switch (option) {
-                    case 1:
-                        System.out.println(Constants.INFO_IF);
-                        break;
-                    case 2:
-                        System.out.println(Constants.INFO_SWITCH);
-                        break;
-                    case 3:
-                        System.out.println(Constants.INFO_FOR);
-                        break;
-                    case 4:
-                        System.out.println(Constants.INFO_WHILE);
-                        break;
-                    case 5:
-                        System.out.println(Constants.INFO_DO_WHILE);
-                        break;
-                    default:
-                         flag = printGoodbyeMsg();
+                    case 1 -> System.out.println(Constants.INFO_IF);
+                    case 2 -> System.out.println(Constants.INFO_SWITCH);
+                    case 3 -> System.out.println(Constants.INFO_FOR);
+                    case 4 -> System.out.println(Constants.INFO_WHILE);
+                    case 5 -> System.out.println(Constants.INFO_DO_WHILE);
+                    default -> flag = printGoodbyeMsg();
                 }
             } catch (InputMismatchException ex) {
                 flag = printGoodbyeMsg();
