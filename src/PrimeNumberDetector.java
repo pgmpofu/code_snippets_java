437622263400e46ca4dec39620905a529cb77b04
techlord <pgmpofu@gmail.com>
2021-04-19 19:52:50 -0700

    Numbers included non-primes

--- src/PrimeNumberDetector.java
+++ src/PrimeNumberDetector.java
@@ -5,19 +5,18 @@
  */
 public class PrimeNumberDetector {
     public static void main(String[] args) {
-        boolean flag = false;
+        boolean isPrime;
         for(int i = 1; i <= 10000; i++) {
-            if(i == 2 || i == 3) {
-                flag = true;
-            }
-            for(int j = 2; j <= i/2; j++) {
-                if(i % j != 0) {
-                    flag = true;
-                }
-                else { flag = false;
+            isPrime = true;
+            for(int j = 2; j < i/2; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
                 }
             }
-            if(flag) { System.out.println(i);}
+            if(isPrime) {
+                System.out.println(i);
+            }
         }
     }
 }
